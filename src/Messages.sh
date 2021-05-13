e22c277922362022aa4fc2077ed07ed7bd523b73
Carl Schwan <carl@carlschwan.eu>
2021-05-14 01:51:15 +0200

    Add Messages.sh

---
+++ src/Messages.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+#SPDX-FileCopyrightText: 2021 Carl Schwan <carl@carlschwan.eu>
+#SPDX-License-Identifier: LGPL-2.0-or-later
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kirigami-addons.pot
