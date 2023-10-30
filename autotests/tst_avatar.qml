trunghieu2570/kirigami

Avatar: Add tests for cyrillic initials
See commit libraries/kirigami-addons@7299162b8b71e3b49e4fef6166432aaa3a6c959c

autotests/tst_avatar.qml
@@ -37,6 +37,19 @@ Item {
             compare(Kirigami.NameUtils.isStringUnsuitableForInitials("蔣經國"), false)
             compare(Kirigami.NameUtils.initialsFromString("蔣經國"), "蔣")
         }
+        function test_cyrillic_name() {
+            compare(Kirigami.NameUtils.isStringUnsuitableForInitials("Нейт Мартин"), false)
+            compare(Kirigami.NameUtils.initialsFromString("Нейт Мартин"), "НМ")
+
+            compare(Kirigami.NameUtils.isStringUnsuitableForInitials("Каланока"), false)
+            compare(Kirigami.NameUtils.initialsFromString("Каланока"), "К")
+
+            compare(Kirigami.NameUtils.isStringUnsuitableForInitials("Зачем кому-то использовать такое длинное не имя в поле Имя"), false)
+            compare(Kirigami.NameUtils.initialsFromString("Зачем кому-то использовать такое длинное не имя в поле Имя"), "ЗИ")
+
+            compare(Kirigami.NameUtils.isStringUnsuitableForInitials("Пользователь Лайв-СИДИ"), false)
+            compare(Kirigami.NameUtils.initialsFromString("Лайв-СИДИ Пользователь"), "ЛП")
+        }
         function test_bad_names() {
             compare(Kirigami.NameUtils.isStringUnsuitableForInitials("151231023"), true)
         }

KDE/kirigami-addons

autotests/tst_avatar.qml
@@ -47,6 +47,19 @@ Item {
             compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("蔣經國"), false)
             compare(KirigamiComponents.NameUtils.initialsFromString("蔣經國"), "蔣")
         }
+        function test_cyrillic_name() {
+            compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("Нейт Мартин"), false)
+            compare(KirigamiComponents.NameUtils.initialsFromString("Нейт Мартин"), "НМ")
+
+            compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("Каланока"), false)
+            compare(KirigamiComponents.NameUtils.initialsFromString("Каланока"), "К")
+
+            compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("Зачем кому-то использовать такое длинное не имя в поле Имя"), false)
+            compare(KirigamiComponents.NameUtils.initialsFromString("Зачем кому-то использовать такое длинное не имя в поле Имя"), "ЗИ")
+
+            compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("Пользователь Лайв-СИДИ"), false)
+            compare(KirigamiComponents.NameUtils.initialsFromString("Лайв-СИДИ Пользователь"), "ЛП")
+        }
         function test_bad_names() {
             compare(KirigamiComponents.NameUtils.isStringUnsuitableForInitials("151231023"), true)
         }