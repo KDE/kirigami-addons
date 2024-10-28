929c17c86dc045e8d599468ca9b7b71bb19ac2cf
Volker Krause <vkrause@kde.org>
2024-10-28 17:03:59 +0100

    Fix exporting the Android integration singleton to QML
    This fixes accessing date/time controls on Android, which broke in 1.5.0.
    
    BUG: 495468

--- src/dateandtime/types.h
+++ src/dateandtime/types.h
@@ -2,14 +2,24 @@
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 #include <qqmlregistration.h>
+#include <QQmlEngine>
 
 #ifdef Q_OS_ANDROID
 #include "androidintegration.h"
 
-struct AndroidIntegrationForeign : public QObject
+struct AndroidIntegrationForeign
 {
-    Q_OBJECT
+    Q_GADGET
+    QML_FOREIGN(KirigamiAddonsDateAndTime::AndroidIntegration)
     QML_NAMED_ELEMENT(AndroidIntegration)
     QML_SINGLETON
+
+public:
+    static KirigamiAddonsDateAndTime::AndroidIntegration *create(QQmlEngine *, QJSEngine *)
+    {
+        auto instance = &KirigamiAddonsDateAndTime::AndroidIntegration::instance();
+        QJSEngine::setObjectOwnership(instance, QJSEngine::CppOwnership);
+        return instance;
+    }
 };
 #endif
