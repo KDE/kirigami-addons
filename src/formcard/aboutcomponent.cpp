f1db7f6503fa3f6fd4d0b454863ea3ac253493c6
Carl Schwan <carl@carlschwan.eu>
2024-11-09 20:16:39 +0100

    Put custom component in before instead of after default components

--- src/formcard/aboutcomponent.cpp
+++ src/formcard/aboutcomponent.cpp
@@ -26,17 +26,19 @@ QList<KAboutComponent> AboutComponent::components() const
     } else {
         platform = QSysInfo::prettyProductName();
     }
-    allComponents.prepend(KAboutComponent(platform, i18nc("@info", "Underlying platform.")));
-    allComponents.prepend(KAboutComponent(i18n("Qt"),
-                                          i18nc("@info", "Cross-platform application development framework."),
-                                          i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
-                                          QStringLiteral("https://www.qt.io/"),
-                                          KAboutLicense::LGPL_V3));
-    allComponents.prepend(KAboutComponent(i18n("KDE Frameworks"),
+    allComponents.append(KAboutComponent(i18n("KDE Frameworks"),
                                           i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt."),
                                           QStringLiteral(KI18N_VERSION_STRING),
                                           QStringLiteral("https://develop.kde.org/products/frameworks/"),
                                           KAboutLicense::LGPL_V2_1));
+
+    allComponents.append(KAboutComponent(i18n("Qt"),
+                                          i18nc("@info", "Cross-platform application development framework."),
+                                          i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
+                                          QStringLiteral("https://www.qt.io/"),
+                                          KAboutLicense::LGPL_V3));
+
+    allComponents.prepend(KAboutComponent(platform, i18nc("@info", "Underlying platform.")));
 
     return allComponents;
 }
