a7d871a11b85738e55aad1a3b016b3cb0a8f74ba
Carl Schwan <carl@carlschwan.eu>
2024-11-09 21:04:47 +0100

    Be slighly more verbose about the plaform

--- src/formcard/aboutcomponent.cpp
+++ src/formcard/aboutcomponent.cpp
@@ -6,6 +6,8 @@
 #include <KLocalizedString>
 #include <QGuiApplication>
 
+using namespace Qt::StringLiterals;
+
 AboutComponent::AboutComponent(QObject *parent)
     : QObject(parent)
 {}
@@ -15,16 +17,22 @@ AboutComponent::~AboutComponent() = default;
 QList<KAboutComponent> AboutComponent::components() const
 {
     QList<KAboutComponent> allComponents = KAboutData::applicationData().components();
+    QString platformName;
     auto platform = QGuiApplication::platformName();
     platform.replace(0, 1, platform[0].toUpper());
-    allComponents.prepend(KAboutComponent(platform, i18n("Windowing system")));
+    if (platform == u"Wayland"_s || platform == u"Xcb"_s) {
+        platform = i18nc("@info Platform name", "%1 (%2)", QSysInfo::prettyProductName(), platform);
+    } else {
+        platform = QSysInfo::prettyProductName();
+    }
+    allComponents.prepend(KAboutComponent(platform, i18nc("@info", "Underlying platform.")));
     allComponents.prepend(KAboutComponent(i18n("Qt"),
-                                          i18nc("@info", "Cross-platform application development framework"),
+                                          i18nc("@info", "Cross-platform application development framework."),
                                           i18n("Using %1 and built against %2", QString::fromLocal8Bit(qVersion()), QStringLiteral(QT_VERSION_STR)),
                                           QStringLiteral("https://www.qt.io/"),
                                           KAboutLicense::LGPL_V3));
     allComponents.prepend(KAboutComponent(i18n("KDE Frameworks"),
-                                          i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt"),
+                                          i18nc("@info", "Collection of libraries created by the KDE Community to extend Qt."),
                                           QStringLiteral(KI18N_VERSION_STRING),
                                           QStringLiteral("https://develop.kde.org/products/frameworks/"),
                                           KAboutLicense::LGPL_V2_1));
